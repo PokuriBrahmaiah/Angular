69488a177b022281222c55101b6d04b5123c0785
PokuriBrahmaiah <brahmaiahpokuri1234@gmail.com>
2023-04-19 20:11:39 +0530

    assignment

---
+++ day 7/day7/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { MenuComponent } from './menu/menu.component';
+MenuComponent
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
